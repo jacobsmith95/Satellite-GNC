f61be8745725c6763830fc14ba7b6f451432f722
Jacob O. Smith <122314435+jacobsmith95@users.noreply.github.com>
2024-06-27 16:50:13 -0700

    added main file and started satellite class

---
+++ satellite.py
@@ -0,0 +1,26 @@
+
+
+class Satellite():
+    """
+    
+    """
+
+    def __init__(self, position, velocity, acceleration):
+        """initializes a satellite object"""
+        self.pos   = position
+        self.vel   = velocity
+        self.accel = acceleration
+        self.orbit = None
+
+        self.trackers = {}
+
+    def move(self, new_position):
+        """moves the satellite object to the new position"""
+        
+
+    def calc_orbit(self):
+        """"""
+
+
+    def run(self):
+        """runs the satellite object"""
