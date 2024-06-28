a48d2882265338be2b13c909e1b7cf52e14a6335
Jacob O. Smith <122314435+jacobsmith95@users.noreply.github.com>
2024-06-27 20:07:39 -0700

    added position movement and run functionality to satellite

--- satellite.py
+++ satellite.py
@@ -1,3 +1,4 @@
+from numpy import polynomial as poly
 
 
 class Satellite():
@@ -5,15 +6,15 @@ class Satellite():
     
     """
 
-    def __init__(self, position, velocity, acceleration):
+    def __init__(self, x_position, y_position, velocity, acceleration):
         """initializes a satellite object"""
-        self.pos   = position
+        self.pos   = (x_position, y_position)
         self.vel   = velocity
         self.acc   = acceleration
-        self.orbit = None
+        self.orbit = poly.Polynomial([y_position, velocity, acceleration])
         self.track = {}
-        self.x_pos = []
-        self.y_pos = []
+        self.x_lst = []
+        self.y_lst = []
 
     def move(self, new_position):
         """moves the satellite object to the new position"""
@@ -22,9 +23,22 @@ class Satellite():
     def calc_orbit(self):
         """"""
 
+    def calc_next_move(self):
+        """calculates the next move for the satellite"""
 
-    def add_position(position):
+    def add_position(self, position):
         """adds x and y coordinates to the self.x_pos and self.y_pos attributes"""
+        self.x_lst.append(position[0])
+        self.y_lst.append(position[1])
 
-    def run(self):
+    def run(self, timing):
         """runs the satellite object; returns self.orbit, self.x_pos, and self.y_pos"""
+        self.add_position(self.pos)
+        for i in range(timing):
+            x = self.pos[0] + 0.25
+            y = self.orbit(x)
+            new_position = (x, y)
+            self.move(new_position)
+            self.add_position(self.pos)
+
+        return (self.orbit, self.x_lst, self.y_lst)
