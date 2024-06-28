4bf485d1d89eb58747f727c68134490b8ae57741
Jacob O. Smith <122314435+jacobsmith95@users.noreply.github.com>
2024-06-27 20:08:16 -0700

    minor comment removal

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ from satellite import Satellite as sat
 
 
 sat1 = sat(0, 1, 1, 1)
-sat_tuple = sat1.run()
+sat_tuple = sat1.run(8)
 func = sat_tuple[0]
 x1 = np.linspace(0, 2, 200)
-y1 = func(x)
+y1 = func(x1)
 x2 = sat_tuple[1]
 y2 = sat_tuple[2]
-
-#t = np.arange(0.0, 2.0, 0.01)
-#s = 1 + np.sin(2 * np.pi * t)
 
 fig, ax = plt.subplots()
 
@@ -22,5 +19,5 @@ ax.scatter(x2, y2)
 fig.suptitle('Satellite Paths')
   
 plt.show()
-print(func)
+print(sat_tuple)
 
