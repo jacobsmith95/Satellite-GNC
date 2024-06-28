59a2770dda7a81effb528226d9ae3eb569fa26c6
Jacob O. Smith <122314435+jacobsmith95@users.noreply.github.com>
2024-06-27 19:54:38 -0700

    plotted satellite orbit from sat object in main

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ import numpy as np
 from satellite import Satellite as sat
 
 
-sat1 = sat()
+sat1 = sat(0, 1, 1, 1)
 sat_tuple = sat1.run()
-x = sat_tuple(0)
-y = sat_tuple(1)
+func = sat_tuple[0]
+x1 = np.linspace(0, 2, 200)
+y1 = func(x)
+x2 = sat_tuple[1]
+y2 = sat_tuple[2]
 
-t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2 * np.pi * t)
+#t = np.arange(0.0, 2.0, 0.01)
+#s = 1 + np.sin(2 * np.pi * t)
 
 fig, ax = plt.subplots()
 
-ax.scatter(x, y)
-ax.plot(t, s)
+ax.plot(x1, y1)
+ax.scatter(x2, y2)
 
 fig.suptitle('Satellite Paths')
   
 plt.show()
+print(func)
 
