8480855f42d12821c30bf6a629188fa49ee4f790
Jacob O. Smith <122314435+jacobsmith95@users.noreply.github.com>
2024-06-27 19:29:35 -0700

    added satellite object to main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from satellite import Satellite as sat
 
+
+sat1 = sat()
+sat_tuple = sat1.run()
+x = sat_tuple(0)
+y = sat_tuple(1)
 
 t = np.arange(0.0, 2.0, 0.01)
 s = 1 + np.sin(2 * np.pi * t)
 
 fig, ax = plt.subplots()
 
-ax.scatter(t, s)
+ax.scatter(x, y)
 ax.plot(t, s)
 
 fig.suptitle('Satellite Paths')
