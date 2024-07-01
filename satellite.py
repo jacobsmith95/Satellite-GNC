06072069eab491560f444246be8a514daffb3455
Jacob O. Smith <jacobsmith95@gmail.com>
2024-07-01 15:21:09 -0700

    changing satellite to estimating its own orbit

--- satellite.py
+++ satellite.py
@@ -6,22 +6,25 @@ class Satellite():
     
     """
 
-    def __init__(self, x_position, y_position, velocity, acceleration):
+    def __init__(self, x_1, y_1, x_2, y_2):
         """initializes a satellite object"""
-        self.pos   = (x_position, y_position)
-        self.vel   = velocity
-        self.acc   = acceleration
-        self.orbit = poly.Polynomial([y_position, velocity, acceleration])
-        self.track = {}
-        self.x_lst = []
-        self.y_lst = []
+        self.curr_pos = (x_2, y_2)
+        self.past_pos = (x_1, y_1)
+        #self.vel      = velocity
+        #self.acc      = acceleration
+        self.orbit    = None
+        self.track    = {}
+        self.x_lst    = []
+        self.y_lst    = []
 
     def move(self, new_position):
         """moves the satellite object to the new position"""
-        self.pos = new_position
+        self.past_pos = self.curr_pos
+        self.curr_pos = new_position
 
     def calc_orbit(self):
         """"""
+        
 
     def calc_next_move(self):
         """calculates the next move for the satellite"""
@@ -35,7 +38,7 @@ class Satellite():
         """runs the satellite object; returns self.orbit, self.x_pos, and self.y_pos"""
         self.add_position(self.pos)
         for i in range(timing):
-            x = self.pos[0] + 0.1
+            x = self.curr_pos[0] + 0.1
             y = self.orbit(x)
             new_position = (x, y)
             self.move(new_position)
