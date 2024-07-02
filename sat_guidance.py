70c85e944638a659b41102fdb6cf0167f4e4ca62
Jacob O. Smith <jacobsmith95@gmail.com>
2024-07-01 20:29:59 -0700

    resolved state vector access

--- sat_guidance.py
+++ sat_guidance.py
@@ -2,27 +2,23 @@ from numpy.polynomial import Polynomial
 
 
 class Guidance():
-    """A class which represents a guidance module for a satellite"""
+    """
+    A class which represents a guidance module for a satellite
+    """
 
     def __init__(self):
-        """
-        
-        """
+        """initializes a Guidance object"""
         self.path     = None
         self.next_pos = None
 
     def determine_path(self, state_vector):
-        """
-        
-        """
-        position_array = state_vector[0]
-        x_array = []
-        y_array = []
-        for element in position_array:
-            x_array.append(element[0])
-            y_array.append(element[1])
+        """"""
+        x_array = [0, 1, 2]
+        y_array = state_vector[0]
+        self.path = Polynomial.fit(x_array, y_array, 1)
 
-        self.path = Polynomial.fit(x_array, y_array, 1)\
+    def determine_next_pos(self):
+        """"""
         
     def get_path(self):
         """returns the determined path"""
