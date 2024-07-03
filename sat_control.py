2838c22c50c4679b429b1ffcdb01f5c7d17c08e0
Jacob O. Smith <jacobsmith95@gmail.com>
2024-07-02 17:22:44 -0700

    added acceleration and state vector method

--- sat_control.py
+++ sat_control.py
@@ -1,6 +1,23 @@
 
 
 class Control():
-    """"""
+    """
+    A class which represents a control module for a satellite
+    """
 
-    
+    def __init__(self):
+        """initializes a Control object"""
+        self.acceleration = None
+        self.state_vector = None
+
+    def calculate_acceleration(self, next_position):
+        """calculates the acceleration needed to reach the next given position"""
+
+
+    def add_state_vector(self, state_vector):
+        """adds the given state vector"""
+        self.state_vector = state_vector
+
+    def get_acceleration(self):
+        """returns acceleration"""
+        return self.accelerationS
