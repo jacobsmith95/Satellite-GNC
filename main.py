e8c3d8be1538da60cd4ecacc7c5cc39e18f5322d
Jacob O. Smith <jacobsmith95@gmail.com>
2024-07-02 17:05:43 -0700

    minor changes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from satellite import Satellite
 sat1 = Satellite()
 sat_tuple = sat1.run([[0, 1, 2]], [[1, 1, 1]], 20)
 func = sat_tuple[0]
-x1 = np.linspace(-10, 30, 200)
+x1 = np.linspace(0, 30, 200)
 y1 = func(x1)
 x2 = sat_tuple[1]
 y2 = sat_tuple[2]
